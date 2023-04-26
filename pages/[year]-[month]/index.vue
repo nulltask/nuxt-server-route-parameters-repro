81a8cd466073e05481c9a847fa6bc8a8e837f07b
Seiya KONNO <nulltask@gmail.com>
2023-04-26 14:49:30 +0900

    initial import

---
+++ pages/[year]-[month]/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const route = useRoute();
+const endpoint = computed(
+  () => `/api/${route.params.year}-${route.params.month}`
+);
+const { data } = await useFetch(endpoint.value);
+</script>
+
+<template>
+  <div>
+    <h1>route.params</h1>
+    <pre>{{ route.params }}</pre>
+    <h1>response (actual)</h1>
+    <pre>{{ data }}</pre>
+    <h1>response (expected)</h1>
+    <pre>{{ route.params }}</pre>
+  </div>
+  <ul>
+    <li><NuxtLink :to="endpoint">endpoint</NuxtLink></li>
+    <li><NuxtLink :to="{ name: 'index' }">index</NuxtLink></li>
+  </ul>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul>
+    <li>
+      <NuxtLink :to="{ name: 'year-month', params: { year: 2023, month: 4 } }"
+        >2023-4</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink :to="{ name: 'year-month', params: { year: 2023, month: 5 } }"
+        >2023-5</NuxtLink
+      >
+    </li>
+  </ul>
+</template>
